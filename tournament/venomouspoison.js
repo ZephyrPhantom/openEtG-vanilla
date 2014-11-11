a207963e8c06ba1468c1f3976a531e91fe9e9d6a
Demur Rumed <gunkmute@gmail.com>
2014-11-11 22:46:52 +0000

    venomouspoison bugfixes

--- tournament/venomouspoison.js
+++ tournament/venomouspoison.js
@@ -4,15 +4,15 @@ module.exports = function(deck) {
 	var bannedCards = etg.NymphList.concat([Cards.NymphsTears.code]);
 	for (var i = 0;i < deck.length;i++) {
 		var card = deck[i];
-		if (free.indexOf(card.code) == -1){
+		if (free.indexOf(card.code) == -1 && (card.type != etg.PillarEnum || card.name.match(/^Mark of/))){
 			if (card.upped) return card.name + " is upgraded. Upgrades are banned";
 			if (~etg.ShardList.indexOf(card.code)) return card.name + " is a shard. Shards are banned";
 			if (~etg.NymphList.indexOf(card.code) || card == Cards.Purify || card == Cards.NymphTears) return card.name + " is banned";
-			if (card.type <= etg.PermanentEnum && perms--){
+			if (card.type <= etg.PermanentEnum && !perms--){
 				return card.name + " would be your 4th non listed permanent";
-			}else if ((card.type == etg.CreatureEnum || card == Cards.Chimera) && creas--){
+			}else if ((card.type == etg.CreatureEnum || card == Cards.Chimera) && !creas--){
 				return card.name + " would be your 4th non listed creature";
-			}else if ((card.type == etg.SpellEnum && card != Cards.Chimera) && spells--){
+			}else if ((card.type == etg.SpellEnum && card != Cards.Chimera) && !spells--){
 				return card.name + " would be your 3rd non listed spell";
 			}
 		}
