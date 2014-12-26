e6b8991bc7a8cb6dce0c17bcbe4b0ab02aa403df
Demur Rumed <gunkmute@gmail.com>
2014-12-26 16:57:20 +0000

    Use browserify API

--- buildetgify.js
+++ buildetgify.js
@@ -1,17 +1,9 @@
 #!/usr/bin/node
-var args = ["--detect-globals=false"];
+var fs = require("fs");
+var browserify = require("browserify");
+var b = browserify([], {detectGlobals: false});
 for(var i=2; i<process.argv.length-1; i++){
-	args.push("-r", "./" + process.argv[i].slice(0, -3));
+	b.require("./" + process.argv[i].slice(0, -3));
 }
-if (process.argv[process.argv.length-1] == "etgify.js") args.push("-r", "engine.io-client")
-args.push("-o", process.argv[process.argv.length-1]);
-function printErr(err){
-	if (err){
-		console.log(err.message);
-	}
-}
-if (process.platform === "win32"){
-	require("child_process").exec("browserify "+args.join(" "), printErr);
-}else{
-	require("child_process").execFile("browserify", args, printErr);
-}
+if (process.argv[process.argv.length-1] == "etgify.js") b.require("engine.io-client");
+b.bundle().pipe(fs.createWriteStream(process.argv[process.argv.length-1]));
