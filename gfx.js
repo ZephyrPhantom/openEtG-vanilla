911f80bb5fd31dfca2e6621ff37970e797cd7fae
Demur Rumed <gunkmute@gmail.com>
2014-12-31 15:10:54 +0000

    Assets renamed

--- gfx.js
+++ gfx.js
@@ -2,14 +2,14 @@
 exports.loaded = false;
 function load(preload, postload){
 	var singles = ["bg_default", "bg_game"];
-	var assets = ["esheet", "backsheet", "cardborders", "statussheet", "statusborders", "typesheet"].concat(singles);
-	var names = {
-		esheet: ["eicons", 32],
-		backsheet: ["cardBacks", 132],
-		cardborders: ["cardBorders", 128],
-		statussheet: ["sicons", 13],
-		typesheet: ["ticons", 25],
-		statusborders: ["sborders", 64],
+	var assets = ["eicons", "cardBacks", "cardBorders", "sicons", "sborders", "ticons"].concat(singles);
+	var widths = {
+		eicons: 32,
+		cardBacks: 132,
+		cardBorders: 128,
+		sicons: 13,
+		ticons: 25,
+		sborders: 64,
 	};
 	var loadingBar = new PIXI.Graphics();
 	preload(loadingBar);
@@ -22,13 +22,13 @@ function load(preload, postload){
 			loadingBar.beginFill(loadCount == assets.length ? 0x336699 : 0xFFFFFF);
 			loadingBar.drawRect(0, 284, 900*loadCount/assets.length, 32);
 			loadingBar.endFill();
-			var obj = names[asset], tex = new PIXI.Texture(new PIXI.BaseTexture(this));
-			if (obj){
-				var ts = [], w = obj[1];
+			var w = widths[asset], tex = new PIXI.Texture(new PIXI.BaseTexture(this));
+			if (w){
+				var ts = [];
 				for (var x = 0; x < tex.width; x += w){
 					ts.push(new PIXI.Texture(tex, new PIXI.Rectangle(x, 0, w, tex.height)));
 				}
-				exports[obj[0]] = ts;
+				exports[asset] = ts;
 			}else exports[asset] = tex;
 			if (loadCount == assets.length){
 				exports.loaded = true;
