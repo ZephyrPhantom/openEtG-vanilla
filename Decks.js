59e4a34f6ad7eb7fed5267dd00980e80bdfc3e19
Fippe94 <Fippe94@spray.se>
2015-07-11 17:15:22 +0200

    Added False Gods

---
+++ Decks.js
@@ -0,0 +1,36 @@
+"use strict";
+exports.fg = [
+	["Akebono", "6rs 6s2 6s2 744 744 744 744 744 744 744 744 744 744 744 744 744 744 744 744 746 746 748 748 748 748 748 748 74a 74c 74g 74g 74i 74i 74i 74i 8pl".split(" ")],
+	["Chaos Lord", "6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6ts 6ts 6ts 6ts 6ts 6ts 6ts 6ts 6ts 6ts 6tt 6tv 6u0 6u0 6u1 6u1 6u2 6u2 6u2 6u3 6u3 6u3 6u3 6u3 6u3 6u4 6u4 6u4 6u5 6u6 6u6 6u6 6u6 74a 7dm 7gn 7jp 7jp 7jp 7jp 7jp 7jp 7t9 8pj".split(" ")],
+	["Dark Matter", "744 744 744 744 744 744 744 744 744 744 744 744 744 744 744 744 744 744 744 745 745 745 745 746 748 74a 74b 74b 74c 74d 74d 74d 74d 74f 74f 74f 74f 74f 74f 74o 74o 7ju 7ju 7ju 7ju 7jv 7k1 8pq".split(" ")],
+	["Decay", "7t6 7t6 7t6 7t6 7t6 7t6 7t8 7t9 7t9 7ta 7tc 7tc 7tc 7tc 7tc 7tc 808 808 808 808 808 808 808 80e 80i 80i 80i 80i 80i 80i 8pt".split(" ")],
+	["Destiny", "6ts 6ts 6ts 6ts 6ts 6ts 6ts 6ts 6ts 6ts 6ts 6ts 6ts 6tt 6tt 6u2 6u2 6u2 6u2 6u2 6u3 6u3 6u3 6u3 6u3 6u6 6u6 7q0 7q0 7q0 7q0 7q0 7q0 7q0 7q0 7q2 7q2 7q2 7q2 7q2 7q2 7q4 7q4 7q4 7q4 7q5 7q5 7q5 7q8 7q8 8ps".split(" ")],
+	["Divine Glory", "7dg 7dg 7dg 7dm 7dm 7dm 7dm 7dm 7dm 7jo 7jo 7jo 7jo 7jo 7jo 7jo 7jo 7jo 7jo 7jo 7jo 7jo 7k1 7k1 7k1 7k1 7k1 7k1 7k2 7k2 7k2 7n2 7n2 7n2 7n2 7n2 8pq".split(" ")],
+	["Dream Catcher", "6ts 6ts 6ts 6ts 6ts 6ts 6tu 6tu 6tu 6tu 6tu 6tu 6u3 6u3 6u3 6u3 6u3 6u3 6u5 6u5 6u8 6u8 6u8 6ug 74f 77i 77j 7dr 7gl 7gn 7n7 7n7 7q1 7q2 7t6 7t6 80a 80h 8pj".split(" ")],
+	["Elidnis", "7ah 7ah 7ah 7ah 7ah 7ah 7ai 7ai 7ak 7al 7am 7am 7am 7am 7am 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gm 7gm 7gm 7gn 7gn 7gn 7gn 7gn 7gu 7gu 7gu 7gv 7gv 7gv 808 808 808 808 808 808 808 80b 80b 80b 80b 80b 80f 80f 80f 80h 80h 8pn".split(" ")],
+	["Eternal Phoenix", "7dg 7dg 7dg 7dg 7dg 7dg 7dg 7dg 7dg 7dg 7dg 7di 7di 7dk 7dk 7dm 7dm 7dm 7dm 7do 7ds 7ds 7ds 7ds 7ds 7ds 80a 80i 80i 80i 8pu".split(" ")],
+	["Ferox", "7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ad 7ad 7ad 7ad 7ae 7ae 7ae 7ae 7af 7af 7af 7af 7af 7af 7ag 7ag 7ag 7ag 7ak 7al 7am 7am 7am 7am 7am 7am 7an 7an 7jp 7jp 7jp 7jp 7jp 7jp 8pn".split(" ")],
+	["Fire Queen", "7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7am 7am 7am 7am 7am 7am 7dk 7dk 7dk 7dn 7dn 7ms 7ms 7ms 7ms 7ms 7ms 7ms 7ms 7ms 7ms 7ms 7n2 7n2 7n2 7n3 7n3 7n3 7n3 7n3 7n3 7n5 7n5 7n5 8pr".split(" ")],
+	["Gemini", "744 744 744 744 744 744 748 748 748 74a 74a 74a 74a 808 808 808 808 808 808 808 808 808 808 808 808 808 80b 80b 80b 80b 80b 80b 80c 80d 80d 80d 80d 80e 80f 80g 80g 80g 80g 80g 80g 8pu".split(" ")],
+	["Graviton", "744 744 744 744 744 744 744 744 744 744 744 744 744 744 745 745 746 749 74a 74a 74a 74b 74b 74b 74c 74d 74e 74e 74e 74e 74e 74e 7dm 7dm 7do 8po".split(" ")],
+	["Hecate", "7dr 7dr 7dr 7dr 7dr 7dr 7t4 7t4 7t4 7t4 7t4 7t4 7t4 7t4 7t4 7t4 7t8 7t9 7t9 7ta 7ta 7tb 7te 7te 7te 7te 7tg 7tg 7tg 7tg 7tg 7tg 7th 8po".split(" ")],
+	["Hermes", "77d 77d 77d 77i 77i 7dg 7dg 7dg 7dg 7dg 7dg 7dg 7dg 7dg 7dg 7dg 7dg 7dg 7dg 7dg 7dg 7dg 7dg 7dg 7dg 7dh 7dh 7dh 7dh 7di 7dj 7dj 7dj 7dj 7dk 7dk 7dk 7dk 7dk 7dk 7dl 7dm 7dm 7dm 7dm 7dm 7dm 7dn 7do 7do 7dq 7dq 7dq 7dq 7dq 7dq 7dr 7dr 8pm".split(" ")],
+	["Incarnate", "712 712 712 712 717 717 717 717 717 71b 71b 71b 71b 7t4 7t4 7t4 7t4 7t4 7t4 7t4 7t4 7t4 7t4 7t4 7t4 7t4 7t4 7t4 7t7 7t7 7t7 7t7 7ta 7ta 7ta 7ta 7tb 7td 7td 7td 7td 7td 7td 8pk".split(" ")],
+	["Jezebel", "6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gp 7h0 7h0 7h0 7h0 7h0 7h0 7t9 7t9 7t9 7t9 7tb 7tc 7tc 7ti 7ti 7ti 8pt".split(" ")],
+	["Lionheart", "6qq 6qq 6qq 7jo 7jo 7jo 7jo 7jo 7k5 7k5 7k5 7k5 7k5 7n2 7q0 7q0 7q0 7q0 7q0 7q0 7q0 7q0 7q3 7q3 7q5 7q5 7q5 7q5 7q5 7q7 7q7 7q7 7q8 7q8 7qc 80h 80h 81q 81q 81q 8ps".split(" ")],
+	["Miracle", "7ac 7ac 7ac 7ac 7ad 7af 7af 7ak 7al 7jo 7jo 7jo 7jo 7jo 7jo 7jo 7jo 7jo 7jq 7jq 7jq 7jr 7jr 7jt 7jv 7jv 7jv 7k1 7k2 7k2 7k2 7ms 7ms 7ms 7ms 7n3 7n3 7n3 8pq".split(" ")],
+	["Morte", "710 710 710 710 710 710 710 710 710 710 710 710 710 710 710 711 711 712 712 712 712 712 712 713 714 714 714 717 717 717 718 718 718 718 718 719 719 719 71a 71a 71b 71b 71b 7jp 7jp 7jp 7jp 7jp 7jp 7ju 7ju 7ju 7k2 8pk".split(" ")],
+	["Neptune", "7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gm 7gm 7gm 7gm 7gn 7gp 7gp 7gr 7gr 7gr 7gr 7gs 7gt 7gt 7h1 7h1 7h1 7h1 7n5 7n7 7n7 7n7 7n7 7n7 7n7 8pr".split(" ")],
+	["Obliterator", "74a 74a 74a 74a 74d 74d 74d 74d 74d 778 778 778 778 778 778 778 778 778 778 778 778 778 778 778 778 778 778 778 778 77b 77b 77b 77b 77b 77c 77c 77f 77f 77h 77h 77h 77h 77h 77h 77i 77i 77i 8pl".split(" ")],
+	["Octane", "7dk 7dk 7dl 7dm 7dm 7ms 7ms 7ms 7ms 7ms 7ms 7ms 7ms 7ms 7ms 7ms 7ms 7n2 7n2 7n2 7n2 7n5 7n5 7n5 7n5 7n5 7n6 7n6 7n6 7n6 7n6 7n6 8po".split(" ")],
+	["Osiris", "74a 74a 74h 74h 752 752 7q0 7q0 7q0 7q0 7q0 7q0 7q0 7q0 7q0 7q0 7q0 7q0 7q0 7q0 7q0 7q0 7q4 7q4 7qb 7qb 7q3 7q8 7qc 7qc 7qc 7qc 7qc 7qc 8pl".split(" ")],
+	["Paradox", "7jp 7jp 7jp 7jp 7jp 7jp 7jp 7jp 7jv 7jv 7jv 7jv 7jv 7jv 7jv 7jv 7k0 7k0 7k1 7k1 7k2 7k2 7k2 7q1 7q1 7q1 7q1 7q1 7q1 7q1 7q1 7q5 7q5 7q5 7q5 808 808 808 808 808 808 808 808 808 808 80b 80b 80b 80b 80b 8ps".split(" ")],
+	["Rainbow", "6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6qq 6u1 6u1 6u1 6u1 74d 74d 74d 77g 77g 77g 77g 7ah 7ah 7ah 7ah 7dm 7dm 7dm 7dm 7gn 7gn 7gn 7k2 7k2 7n5 7n5 7q5 7q5 7q5 7q5 7q5 7t9 7t9 7t9 80a 80a 8ps".split(" ")],
+	["Scorpio", "718 718 718 718 718 71a 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gk 7gl 7gl 7gl 7gm 7gm 7gm 7gn 7gn 7gn 7go 7gp 7gp 7gq 7gr 7gr 7gs 7gt 7gu 7gu 7gu 7gu 7gu 7gu 7gv 7gv 808 808 808 80b 80b 8pk".split(" ")],
+	["Seism", "778 778 778 778 778 778 778 778 778 778 778 778 778 778 778 778 779 77a 77a 77b 77b 77c 77e 77e 77e 77f 77g 77g 77g 77g 77g 77g 77i 77i 77j 77j 77j 77j 77j 77j 77k 7q4 7q4 7q4 7q4 7q6 8ps".split(" ")],
+	["Serket", "710 710 710 710 710 710 710 713 713 713 713 713 71a 71a 71e 71e 71e 71e 71e 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ac 7ao 7ao 7ao 7ao 7ao 7b0 7b0 7ta 7ta 7ta 7ti 7ti 7ti 8pt".split(" ")]
+];
+
+exports.giveRandom = function() {
+	return exports.fg[Math.floor(Math.random() * exports.fg.length)];
+}
