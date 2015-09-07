c9cb16c8eb120dd2acb3f026bab4d3dea34ba8a5
Demur Rumed <gunkmute@gmail.com>
2015-09-07 13:41:20 +0000

    Drop urllib

--- updatedb.js
+++ updatedb.js
@@ -1,12 +1,22 @@
 #!/usr/bin/node
 "use strict";
-var fs = require("fs"), urllib = require("urllib");
+var fs = require("fs"), zlib = require("zlib"), http = require("https");
 function download(gid, writeStream, cb){
-	urllib.request("https://docs.google.com/spreadsheets/d/15G8h9S0Ph7UfzaZMyn35LpJY8gTgmTtru6K2QmdTUmw/export?format=csv&id=15G8h9S0Ph7UfzaZMyn35LpJY8gTgmTtru6K2QmdTUmw&gid="+gid, {
-		headers: { "GData-Version": "3.0" },
-		writeStream: writeStream,
-		gzip: true
-	}, cb);
+	http.get({
+		host:"docs.google.com",
+		path:"/spreadsheets/d/15G8h9S0Ph7UfzaZMyn35LpJY8gTgmTtru6K2QmdTUmw/export?format=csv&id=15G8h9S0Ph7UfzaZMyn35LpJY8gTgmTtru6K2QmdTUmw&gid="+gid,
+		headers: { "GData-Version": "3.0", "Accept-Encoding:gzip" },
+	}, function(res){
+		if (res.headers["Content-Encoding"] == "gzip"){
+			res = res.pipe(zlib.createGunzip());
+		}
+		res.pipe(writeStream);
+		res.on("end", function(){
+			cb(null);
+		});
+	}).on("error", function(err){
+		cb(err);
+	});
 }
 var dbgid = [
 	["pillar", "0"],
@@ -19,7 +29,7 @@ var dbgid = [
 ];
 dbgid.forEach(function(pair){
 	if (process.argv.length == 2 || process.argv.some(function(x) { return x.indexOf(pair[0]) == 0; })){
-		download(pair[1], fs.createWriteStream(pair[0]+".csv"), function(err, data, res){
+		download(pair[1], fs.createWriteStream(pair[0]+".csv"), function(err){
 			if (err){
 				console.log("Failed to download " + pair[0], err.message);
 			}else console.log(pair[0]);
